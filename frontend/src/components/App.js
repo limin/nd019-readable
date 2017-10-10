312785302b1b9c19034580f027099b431906e5d6
Min Li <limin9@gmail.com>
2017-10-10 02:25:59 +0000

    add post and category views

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -24,9 +24,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="top-bar">
+      		<span className="logo">
+          		<img src={logo} className="App-logo" alt="logo" />
+      		</span>
+      		<ul className="nav-bar">
+      			<li><a>Posts</a></li>
+      			<li><a>Categories</a></li>
+      		</ul>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
